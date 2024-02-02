235f4f9db9cde64ec2b93a1967cdf57480758e96
BeñatIturrioz <ben_itu_muj@goierrieskola.net>
2024-02-02 14:09:47 +0100

    convert local variable to field

--- src/erronka/Panela.java
+++ src/erronka/Panela.java
@@ -12,6 +12,7 @@ public class Panela extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JButton btnBiltegia;
 
 	public Panela(String user) {
 		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
@@ -44,15 +45,8 @@ public class Panela extends JFrame {
 					}else System.out.println("Baimena ukatua");
 			}
 		});
-<<<<<<< HEAD
-		new JButton("Hornitzailea").setBounds(258, 0, 216, 101);
-		contentPane.add(new JButton("Hornitzailea"));
-=======
-		btnHornitzailea.setBounds(258, 0, 266, 191);
-		contentPane.add(btnHornitzailea);
->>>>>>> b982bab9d706937193e80286850f900c2357fd42
 		
-		JButton btnBiltegia = new JButton("Biltegia");
+		btnBiltegia = new JButton("Biltegia");
 		btnBiltegia.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if(user.equals("Laura")||user.equals("Beñat")||user.equals("Martín")||user.equals("Miguel")||user.equals("Javier")||user.equals("Sara")||user.equals("Sergio")||user.equals("Daniel")||user.equals("Juan")) {
